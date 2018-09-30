f99f5f679ce84261fddee1301e8791fff0c78532
https://github.com/sallyFishKun/loadmoreAndRefresh.git <309276974@qq.com>
2018-09-30 17:22:23 +0800

    0

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,119 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+let isRefesh = false; //正在下拉更多
+let isloadmore = false; //正在下载更多
+let istop = true;
+let pageSize = 10;
+let pageIndex = 1;
+let totalpages = 1;
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    tabs: ["选项一", "选项二"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    wH: 500,
+    mydata:[0,1,2,3],
+    isTop: true, //滚动条顶部
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          wH: res.windowHeight - 60,
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  
+  },
+  /**
+  * 页面相关事件处理函数--监听用户下拉动作
+  */
+  onPullDownRefresh: function () {
+
+    isRefesh = true;
+    isloadmore = false;
+    istop = false;
+    wx.showToast({
+      title: 'loading...',
+      icon: 'loading'
+    })
+    let self = this;
+    setTimeout(() => {
+      pageIndex++;
+      self.getData();
+
+    }, 2000)
+  },
+  stopPullDownRefresh: function () {
+    wx.stopPullDownRefresh({
+      complete: function (res) {
+        wx.hideToast()
+        console.log(res, new Date())
+      }
+    })
+  },
+  //滚动条到top
+  doScrollTop(e) {
+    istop = this.data.isTop
+    if (e.detail.scrollTop < 10) {
+
+      if (!istop) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          isTop: true
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+      if (istop) {
+        this.setData({
+          isTop: false
+        })
+      }
+
     }
+   
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  /**
+   * 更多
+   */
+  loadmore(){
+    let self=this;
+    wx.showLoading({
+      title: 'loading',
     })
-  }
-})
+    setTimeout(() => {
+      pageIndex++;
+      
+      isloadmore = true;
+      self.getData();
+     
+    }, 2000)
+  },
+  getData(){
+    wx.hideLoading();
+    let mydata = this.data.mydata;
+    if(isRefesh){
+      this.stopPullDownRefresh();
+      mydata = [0, 1, 2, 3];
+    }else{
+     
+      for (let i = 0; i < 5; i++) {
+        mydata.push(i)
+      }
+    }
+    this.setData({
+      mydata
+    })
+    isRefesh = false;
+    isloadmore = false;
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+ 
+});
